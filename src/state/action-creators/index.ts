b5d152c1b472f7f46c056b4f63a2297554efe2f8
Ji99 <rksk0531@naver.com>
2022-09-05 11:29:30 +0900

    commit

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,29 @@
+import { ActionType } from "../action-types";
+import { Dispatch } from "redux";
+
+export const depositMoney = (amount: number) => {
+  return (dispatch: Dispatch) => {
+    dispatch({
+      type: ActionType.DEPOSIT,
+      payload: amount,
+    });
+  };
+};
+
+export const withdrawMoney = (amount: number) => {
+  return (dispatch: Dispatch) => {
+    dispatch({
+      type: ActionType.WITHDRAW,
+      payload: amount,
+    });
+  };
+};
+
+export const bankrupt = (amount: number) => {
+  return (dispatch: Dispatch) => {
+    dispatch({
+      type: ActionType.BANKRUPT,
+      payload: amount,
+    });
+  };
+};
